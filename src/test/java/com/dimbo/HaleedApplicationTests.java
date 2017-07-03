b744135867b9e241b815482a1f07f3c0f41329aa
Dmitiriy Cherkashyn <dmitriy.cherkashyn@gmail.com>
2017-07-03 11:17:59 +0300

    initial commit

---
+++ src/test/java/com/dimbo/HaleedApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dimbo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class HaleedApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//		mock
+//	}
+//
+//}
